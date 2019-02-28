dfe65681c43396310c3761f712f84e9a0884f935
Renan <renan_fenger_93@hotmail.com>
2019-02-28 16:08:18 -0300

    Updates

---
+++ precache-manifest.9117f0b10f935a1685f07b10287e7e28.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "164859968039ea9eb920",
+    "url": "/static/css/main.eb814a60.chunk.css"
+  },
+  {
+    "revision": "164859968039ea9eb920",
+    "url": "/static/js/main.16485996.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "125fd37daf4d2807f2ed",
+    "url": "/static/css/2.a3b04f89.chunk.css"
+  },
+  {
+    "revision": "125fd37daf4d2807f2ed",
+    "url": "/static/js/2.125fd37d.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "14f604578c71140a0862bc6449e5ee70",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app-shoppingcart2-test/precache-manifest.d2743b5671465123ef00d1c0ae3e4773.js"
+  "/precache-manifest.9117f0b10f935a1685f07b10287e7e28.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-app-shoppingcart2-test/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
